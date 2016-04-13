b85acbcce8dc0ba800b4b3e1084958a93d52a3f6
Yuchen Wang <ycwang0712@gmail.com>
2016-04-12 21:54:29 -0400

    adding data as internal

---
+++ R/data_doc.R
@@ -0,0 +1,27 @@
+#' Example datasets
+#'
+#' These are the datasets of case and controls, each have two replicates. Case
+#' 1 and case 2 have 4714 and 5197 observations and control 1 and control 2
+#' have 1817 and 5249 observations respectively.
+#'
+#' @format Both of the data sets are data frame with 7 variables:
+#' \describe{
+#'   \item{chrBase}{Factor}
+#'   \item{chr}{Factor with 1 level "chr1".}
+#'   \item{base}{integer ...}
+#'   \item{strand}{Factor with 2 levels "F" and "R".}
+#'   \item{coverage}{integer ...}
+#'   \item{freqC}{numeric ...}
+#'   \item{freqT}{numeric ...}
+#' }
+#' @rdname demo_data
+"case1"
+
+#' @rdname demo_data
+"case2"
+
+#' @rdname demo_data
+"control1"
+
+#' @rdname demo_data
+"control2"
